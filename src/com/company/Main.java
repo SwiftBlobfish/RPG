c9e031f16eaf55c96b07ac46b64eeee28d5210bb
Andy <hayesandy5@gmail.com>
2018-05-03 23:16:13 -0400

    Upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main
                Weapon sword= new Weapon("Sword",10);
                System.out.println(sword);
                Weapon spear = new Weapon("Spear",5);
+               System.out.println(spear);
            }
     }
